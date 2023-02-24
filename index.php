5ff06d6fb343ba1ed2e1d0ae85d8d1caa5a89786
ExTazaa <53406281+ExTazaa@users.noreply.github.com>
2023-02-24 18:27:16 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<head>
+	<title>Pomocnik Event Schwytaj Króla</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="event,event metin2,karty,karta,drop kart,jak grać,metin2,otwarcia,brązowa,srebrna,złota,szkatułka,szkata,talia,pomoc,schwytaj króla">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" type="text/css" href="https://cs-kurnik.pl/extaza/schwytaj/template.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js" /></script>
+</head>
+<body>
+
+<div>
+<?php include "./event_king.html" ?>
+</div>
+
+</body>
